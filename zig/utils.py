d3ed1eaa67044007e703fc27dea6e9b527b67af0
Whit Morriss <whit@surveymonkey.com>
2012-03-05 10:36:18 -0600

    add dump from zguide

--- zig/utils.py
+++ zig/utils.py
@@ -1,4 +1,7 @@
 from functools import wraps
+import binascii
+import zmq
+
 
 def coro(func):
     @wraps(func)
@@ -7,3 +10,26 @@ def coro(func):
         cr.next()
         return cr
     return start
+
+
+def dump(msg_or_socket):
+    # from zguide zhelpers
+    if isinstance(msg_or_socket, zmq.Socket):
+        # it's a socket, call on current message
+        return dump(msg_or_socket.recv_multipart())
+    else:
+        msg = msg_or_socket
+    print "----------------------------------------"
+    for part in msg:
+        print "[%03d]" % len(part),
+        is_text = True
+        for c in part:
+            if ord(c) < 32 or ord(c) > 128:
+                is_text = False
+                break
+        if is_text:
+            # print only if ascii text
+            print part
+        else:
+            # not text, print hex
+            print binascii.hexlify(part)
